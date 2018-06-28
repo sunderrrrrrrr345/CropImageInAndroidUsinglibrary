2374c81d62acaa2642fd7b35ac9ba77b233bf511
Sunder <sunderrrrrrrr345@users.noreply.github.com>
2018-06-28 14:57:00 +0530

    Add files via upload

---
+++ app/src/main/java/integration/google/sunder/cropimageinandroidusinglibrary/APIUrl.java
@@ -0,0 +1,5 @@
+package integration.google.sunder.cropimageinandroidusinglibrary;
+
+public class APIUrl {
+    public static final String BASE_URL = "http://iiieyelobby.com/aval-fashion/admin/uploads/";
+}
--- app/src/main/java/integration/google/sunder/cropimageinandroidusinglibrary/MainActivity.java
+++ app/src/main/java/integration/google/sunder/cropimageinandroidusinglibrary/MainActivity.java
@@ -18,12 +18,14 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -39,14 +41,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageButton imageButton;
     Uri imageUri;
-
+    TextView textView;
+CropImage.ActivityResult result;
     /**
      * Persist URI image to crop URI if specific permissions are required
      */
+    String encodedImageonCapture;
     private Uri mCropImageUri;
     private static final int PERMISSION_REQUEST_CODE = 1;
     @Override
@@ -54,6 +68,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageButton = findViewById(R.id.quick_start_cropped_image);
+        textView=findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(encodedImageonCapture!=null){
+                    Toast.makeText(getApplicationContext(),"Uploaded",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Please Upload file",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
         // handle result of pick image chooser
         if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             imageUri = CropImage.getPickImageResultUri(this, data);
-            Log.i("Image_details11111111111111111111111", "" + data.getData());
+//            Log.i("Image_details11111111111111111111111", "" + data.getData());
             //convert file data to bystes
        /*  */
 
@@ -160,11 +185,12 @@ public class MainActivity extends AppCompatActivity {
 
         // handle result of CropImageActivity
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+             result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 ((ImageButton) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
+                Upload_documents(result);
+                Log.i("Result_uri",""+result.getUri());
                 Toast.makeText(this, "Cropping successful, Sample: " + result.getUri(), Toast.LENGTH_LONG).show();
-
                 try {
                     URL url = new URL(result.getUri().toString());
                     try {
@@ -172,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
                         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
                         bitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
                         byte[] b1 = bytes.toByteArray();
-                        String encodedImageonCapture = Base64.encodeToString(b1, Base64.DEFAULT);
+                        encodedImageonCapture = Base64.encodeToString(b1, Base64.DEFAULT);
                         Log.i("Image_details", "" + encodedImageonCapture);
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -187,8 +213,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-     private void Bytes_File(Uri data1) {
+    private void Upload_documents(CropImage.ActivityResult result) {
+        File file = new File(result.toString());
+        //   dialog.setProgress(0);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(APIUrl.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+   /*   dialog.setMessage("Uploading " + file.getName()+"/"+file.length());
+        dialog.show();*/
+        Api as = retrofit.create(Api.class);
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), result.toString());
+        //ProgressRequestBody fileBody = new ProgressRequestBody(file, this);
+        MultipartBody.Part multipartBody = MultipartBody.Part.createFormData("file", file.getName(), requestFile);
+        Log.i("requestFile", requestFile.toString());
+        Log.i("requestFile1", "" + multipartBody.toString());
+        Log.i("requestFile1", "" + file.getName());
+        Call<ResponseBody> responseBodyCall = as.addRecord("afddf", 1325, "fileName", multipartBody);
+        Log.i("requestFile1", "" + responseBodyCall.toString());
+        responseBodyCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                Log.d("Success", "success " + response.code());
+                Log.d("Success", "success " + response.message());
+                Toast.makeText(getApplicationContext(),"yes",Toast.LENGTH_SHORT).show();
+                //    dialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                Log.d("failure", "message = " + t.getMessage());
+                Log.d("failure", "cause = " + t.getCause());
+                Toast.makeText(getApplicationContext(),"no",Toast.LENGTH_SHORT).show();
+                //   dialog.dismiss();
+            }
+        });
+    }
+
+
+    private void Bytes_File(Uri data1) {
          //Uri data1 = data.getData();
+        Upload_documents1(data1);
+        Toast.makeText(this, "File12345", Toast.LENGTH_SHORT).show();
          ByteArrayOutputStream baos = new ByteArrayOutputStream();
          FileInputStream fis;
          try {
@@ -202,9 +268,47 @@ public class MainActivity extends AppCompatActivity {
              e.printStackTrace();
          }
          byte[] bbytes = baos.toByteArray();
-         String encodedImageonCapture = Base64.encodeToString(bbytes, Base64.DEFAULT);
-         Log.i("Image_details11111111111111111111111", "" + encodedImageonCapture);
+          encodedImageonCapture = Base64.encodeToString(bbytes, Base64.DEFAULT);
+         Log.i("Image_details11111111111111111111111111111111", "" + encodedImageonCapture);
      }
+
+    private void Upload_documents1(Uri data1) {
+
+        File file = new File(data1.toString());
+        //   dialog.setProgress(0);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(APIUrl.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+   /*   dialog.setMessage("Uploading " + file.getName()+"/"+file.length());
+        dialog.show();*/
+        Api as = retrofit.create(Api.class);
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), data1.toString());
+        //ProgressRequestBody fileBody = new ProgressRequestBody(file, this);
+        MultipartBody.Part multipartBody = MultipartBody.Part.createFormData("file", file.getName(), requestFile);
+        Log.i("requestFile", requestFile.toString());
+        Log.i("requestFile1", "" + multipartBody.toString());
+        Log.i("requestFile1_sayamu", "" + file.getName());
+        Call<ResponseBody> responseBodyCall = as.addRecord("afddf", 1325, "fileName", multipartBody);
+        Log.i("requestFile1", "" + responseBodyCall.toString());
+        responseBodyCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                Log.d("Success", "success " + response.code());
+                Log.d("Success", "success " + response.message());
+                Toast.makeText(getApplicationContext(),"yes",Toast.LENGTH_SHORT).show();
+                //    dialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                Log.d("failure", "message = " + t.getMessage());
+                Log.d("failure", "cause = " + t.getCause());
+                Toast.makeText(getApplicationContext(),"no",Toast.LENGTH_SHORT).show();
+                //   dialog.dismiss();
+            }
+        });
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
